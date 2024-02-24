751043232d6471ddce1850a9b6d4f6f331d72c54
Joel Stransky <stranskydesign@gmail.com>
2024-02-23 22:25:49 -0800

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,17 @@ import { defineConfig, squooshImageService } from "astro/config";
 import robots from "astro-robots";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://minimma.vercel.app",
+  site: "https://joelstranky.netlify.app",
   image: {
     service: squooshImageService()
   },
   integrations: [tailwind(), sitemap(), robots(), mdx()],
   output: "server",
-  adapter: vercel()
+  adapter: netlify()
 });
